9107b1c45c954e4e5abd1ddc826aa4172f4521be
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2024-10-09 21:36:37 +0600

    Ready to build the project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        dark1: "#100F0F",
+        dark2: "#403F3F",
+        dark3: "#706F6F",
+        modalBtnBg: "#DC3545",
+        BtnBg: "#FFC107",
+      },
+      fontFamily: {
+        Poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
